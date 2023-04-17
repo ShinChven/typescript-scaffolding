ff7c6f7dda84b339cb9f5fa7fccedefbe2f0ced4
SHINCHVEN <shinchven@gmail.com>
2023-04-17 16:07:09 +0800

    Update ts-init.sh

--- ts-init.sh
+++ ts-init.sh
@@ -1,6 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 set -x
+
+# Check if a directory name was provided
+if [ -z "$1" ]
+then
+  echo "Please provide a directory name."
+  exit 1
+fi
+
+# Clone the repository
 git clone https://github.com/ShinChven/typescript-scaffolding.git "$1"
+
+# Change to the directory
 cd "$1"
-rm -rf .git ts-init.sh
-git init
+
+# Remove the .git directory
+rm -rf .git
+
+# Update the name in package.json
+sed -i "s/\"name\": \"typescript-scaffolding\"/\"name\": \"$1\"/" package.json
+
+# Install npm dependencies
+npm install
